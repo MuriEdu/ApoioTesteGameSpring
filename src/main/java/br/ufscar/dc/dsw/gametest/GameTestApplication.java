3b1bcf43c6a56460d863a39cbc00ead0b1418760
MuriEdu <muriloeduardo6@gmail.com>
2025-06-02 21:25:52 -0300

    init project

---
+++ src/main/java/br/ufscar/dc/dsw/gametest/GameTestApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.gametest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufscar/dc/dsw/gametest/GameTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.gametest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
